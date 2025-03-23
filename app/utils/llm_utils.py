ecb0ddcd7c9d27a1fae1087f622a707ddde495f9
glebsolyanik <glebsolyanik03@gmail.com>
2025-03-23 12:47:46 +0300

    add langraph memory for agent(LLMAgent)

--- app/utils/llm_utils.py
+++ app/utils/llm_utils.py
@@ -1,20 +1,50 @@
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
+from openai import OpenAIError
+import time
+class LLMAgent:
+    def __init__(self, model, model_provider, api_url, api_key):
+        self.llm = init_chat_model(
+            model=model,
+            model_provider=model_provider,
+            base_url=api_url,
+            api_key=api_key,
+        )
 
+        self.graph = self.create_graph()
+    
+    def call_model(self, state: MessagesState):
+        response = self.llm.invoke(state["messages"])
+        return {"messages": response}
 
-def send_message_llm(context_arr, API_URL, MODEL_NAME,temperature):
-    model = ChatOpenAI(base_url=API_URL, model=MODEL_NAME, temperature=temperature)
-    message = ""
-    for m in context_arr:
-        message = message + f'Role:{m["role"]} Content: {m["content"]}\n'
-    if model.get_num_tokens(message) >= 32000:
-        while model.get_num_tokens(message) >= 32000:
-            context_arr.popleft()
-            message = ""
-            for m in context_arr:
-                message = message + f'Role:{m["role"]} Content: {m["content"]}\n'
-    content = model.invoke(
-        input=message,
-    ).content
-
-    return content
-
+    def create_graph(self):
+        builder = StateGraph(state_schema=MessagesState)
+        builder.add_node('call_model', self.call_model)
+        builder.add_edge(START, 'call_model')
+        graph = builder.compile(checkpointer=MemorySaver())
+        return graph
+    
+    def validate_model(self):
+        try:
+            self.llm.invoke("test")
+            return True
+        except OpenAIError:
+            return False
+        
+    def send_message(self, messages, temperature, chat_id):
+        self.llm.temperature = temperature
+        stream = self.graph.stream(
+            input={"messages": messages}, 
+            config= {
+                "configurable": {
+                    "thread_id": chat_id,
+                    }
+            },
+            stream_mode="messages"
+        )
+        
+        for msg, metadata in stream:
+            yield msg.content
+            time.sleep(0.02)
